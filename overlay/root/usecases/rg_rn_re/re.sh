14381b04f1560b993cb6d37b97db5a771a8e5808
anon <anon@example.com>
2026-02-24 17:08:46 +0000

    rm stale stuff

--- overlay/root/usecases/rg_rn_re/re.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-"$SCRIPT_DIR/re_setup.sh"
-"$SCRIPT_DIR/re_app.sh"
--- overlay/root/usecases/rg_rn_re/rg.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-"$SCRIPT_DIR/rg_setup.sh"
-"$SCRIPT_DIR/rg_app.sh"
